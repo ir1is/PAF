6da0e0bd773ac03a4ffc7a2a535ca7d74cc93c84
ir1is <100912962+ir1is@users.noreply.github.com>
2022-03-15 22:34:05 +0100

    1

---
+++ vjezbe_2/1.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+t = []
+x= []
+v= []
+a = []
+
+def gibanje(F,m):
+    a.append(F/m)
+    dt = 0.1
+    t.append(0)
+    x.append(0)
+    v.append(0)
+    for i in range(0,10):
+        t.append(i * dt)
+        v.append(v[i]+ a[i]*dt)
+        x.append( x[i]+v[i]* dt)
+        a.append(F/m)
+
+    plt.plot(t,x, label= 'x-t')
+    plt.plot(t,v, label= 'v-t')
+    plt.plot(t,a, label='a-t')
+    plt.show()
+
+gibanje(100,10)
