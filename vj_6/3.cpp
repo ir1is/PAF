af7fa8f93e4d0efad6c51f2072c49f1da79c19df
ir1is <100912962+ir1is@users.noreply.github.com>
2022-06-20 00:02:50 +0200

    Ispravak 6

--- vj_6/3.cpp
+++ vj_6/3.cpp
@@ -13,28 +13,42 @@ void polje(list <int> lista){
     
 }
 
+void interval(list<int> lista, int a, int b) {
+    
+    list <int> :: iterator it;
+    for(it = lista.begin(); it != lista.end(); ++it)
+        if (a <= *it && *it <= b) {
+            cout << '\t' << *it;
+        }
+    cout << '\n'; 
+}
+
 int main() {
 
     list<int> lista;
     int a = 1;
     int b = 9;
-    lista.push_back(a);
+
     lista.push_back(3);
+    lista.push_back(0);
+    lista.push_back(5);
     lista.push_back(7);
     lista.push_back(2);
-    lista.push_back(5);
-    lista.push_back(b);
+    lista.push_back(12);
+    lista.push_back(4);
 
     cout << "\nOriginalna lista: ";
     polje(lista);
+
+    cout << "\nElementi liste iz intervala [a,b]: ";
+    interval(lista, a, b);
 
     cout << "\nLista nakon mijenjanja redoslijeda: ";
     lista.reverse();
     polje(lista);
 
-    cout << "\nLista nakon zamjene: ";
-    std::swap(a, b);
-    polje(lista);
+    cout << "\nLista nakon zamjene 1. i 3. elementa: ";
+    
 
     cout << "\nLista nakon sortiranja po velicini : ";
     lista.sort();
